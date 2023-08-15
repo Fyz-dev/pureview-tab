632f62349554e432c0bafe4334aba273be20d13c
FYZ <63561158+Vladhuman@users.noreply.github.com>
2023-08-16 01:02:05 +0300

    Added animation for open/close

--- src/layouts/PanelSettings/GroupSetting/GroupSetting.jsx
+++ src/layouts/PanelSettings/GroupSetting/GroupSetting.jsx
@@ -1,18 +1,32 @@
 import { useState } from 'react';
 import styles from './GroupSetting.module.css';
+import { useSpring, animated } from 'react-spring';
 
 const GroupSetting = ({ name, children }) => {
   const [isOpen, setIsOpen] = useState(false);
+  //Отвечает за то, отображено ли содержимое GroupSetting
+  const [displayChildren, setDisplayChildren] = useState(false);
+
+  const containerAnimation = useSpring({
+    transform: isOpen ? 'translateY(0px)' : 'translateY(-100%)',
+    reset: true,
+    onRest: () => {
+      if (displayChildren && !isOpen) setDisplayChildren(false);
+    },
+  });
 
   return (
-    <div className={isOpen && `${styles['main-container-open']}`}>
+    <div>
       <button
-        // При закрытие добавляем подсветку при наведении мыши
+        // Когда children полностью скрыты - меняем
         className={`${styles.button} ${
-          !isOpen && styles['button-group-setting-close']
+          displayChildren
+            ? styles['button-when-open']
+            : styles['button-group-setting-close']
         }`}
         onClick={() => {
           setIsOpen(!isOpen);
+          setDisplayChildren(true);
         }}
       >
         <span className={styles.text}>{name}</span>
@@ -26,7 +40,16 @@ const GroupSetting = ({ name, children }) => {
           chevron_right
         </span>
       </button>
-      {isOpen && <div className={styles['container-children']}>{children}</div>}
+      {displayChildren && (
+        <div className={styles['container-anim']}>
+          <animated.div
+            style={containerAnimation}
+            className={styles['container-child-when-open']}
+          >
+            <div className={styles['container-children']}>{children}</div>
+          </animated.div>
+        </div>
+      )}
     </div>
   );
 };
--- src/layouts/PanelSettings/PanelSetting.jsx
+++ src/layouts/PanelSettings/PanelSetting.jsx
@@ -5,34 +5,19 @@ import styles from './PanelSetting.module.css';
 const PanelSetting = () => {
   return (
     <PanelDark className={styles.container}>
-      <GroupSetting name="Clock">
-        <div style={{ display: 'flex' }}>
-          <span>Align:</span>
-          <button
-            style={{ width: '100%', height: '20px' }}
-            type="button"
-          ></button>
-        </div>
-        <div style={{ display: 'flex' }}>
-          <span>Align:</span>
-          <button
-            style={{ width: '100%', height: '20px' }}
-            type="button"
-          ></button>
-        </div>
-      </GroupSetting>
+      <GroupSetting name="Clock"></GroupSetting>
       <GroupSetting name="Wallpaper">
         <div style={{ display: 'flex' }}>
-          <span>Align:</span>
+          <span style={{ fontSize: '18px' }}>Align:</span>
           <button
-            style={{ width: '100%', height: '20px' }}
+            style={{ width: '100%', height: '20px', fontSize: '24px' }}
             type="button"
           ></button>
         </div>
         <div style={{ display: 'flex' }}>
-          <span>Align:</span>
+          <span style={{ fontSize: '18px' }}>Align:</span>
           <button
-            style={{ width: '100%', height: '20px' }}
+            style={{ width: '100%', height: '20px', fontSize: '24px' }}
             type="button"
           ></button>
         </div>
